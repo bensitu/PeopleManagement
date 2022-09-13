653122dbb92cab62bfeaa3dd76df61bd07fb8269
BenSitu <binofsitu@gmail.com>
2022-09-13 21:20:08 +0900

    First commit

---
+++ out/artifacts/PeopleManagement_Web_exploded/src/main/java/com/northsea/peoplemanagement/PeopleManagementApplication.java
@@ -0,0 +1,14 @@
+package com.northsea.peoplemanagement;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeopleManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PeopleManagementApplication.class, args);
+    }
+
+}
